460e87a701c382315b1859ffadc325fc28eb88a5
João Vitor Dadas <dadasjv@hotmail.com>
2022-09-01 18:08:27 -0300

    Criando Primeiro app em React

---
+++ estudos-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'Matheus'
+  
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <p>Olá {name}</p>
+      
+    </div>
+  );
+}
+
+export default App;
