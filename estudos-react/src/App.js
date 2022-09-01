18335ffb6e28d27b203d2680206625f626319e5e
João Vitor Dadas <dadasjv@hotmail.com>
2022-09-01 18:30:44 -0300

    Components

--- estudos-react/src/App.js
+++ estudos-react/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWord from './components/HelloWord';
 
 function App() {
   const name = 'Matheus'
-  
+
   return (
     <div className="App">
       <h1>Olá React</h1>
       <p>Olá {name}</p>
-      
+      <HelloWord/>
     </div>
   );
 }
