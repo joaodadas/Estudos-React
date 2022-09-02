6bf0a38dc16475e6a87a3c2682b8063a30a294a8
João Vitor Dadas <dadasjv@hotmail.com>
2022-09-01 22:26:57 -0300

    sla

--- estudos-react/src/App.js
+++ estudos-react/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <SayMyName nome="João"/>
       <SayMyName nome={nome}/>
       <Pessoa nome={"João"} idade={19} profissao={"Engenheiro de Software"} fot="https://via.placeholder.com/150"/>
-
+    <List/>
     </div>
   );
 }
---
+++ estudos-react/src/components/List.js
@@ -0,0 +1,15 @@
+import Item from './Item'
+
+function List() {
+    return (
+        <>
+            <h1>Minha Lista</h1>
+            <ul>
+                <Item marca="Bulgat"/>
+                <Item marca="Jaquar"/>
+            </ul>
+        </>
+    )
+}
+
+export default List
