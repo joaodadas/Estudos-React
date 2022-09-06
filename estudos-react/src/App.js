5eaf803109d6f67bf68ba3dbf80a41473ddf46fb
João Vitor Dadas <dadasjv@hotmail.com>
2022-09-06 15:33:51 -0300

    delete

--- estudos-react/src/App.js
+++ estudos-react/src/App.js
@@ -1,14 +1,23 @@
-import './App.css'
-import Condicional from './components/Condicional'
+import {Routes, Route, Router } from "react-router-dom";
+import Home from './pages/Home'
+import Empresa from './pages/Empresa'
+import Contato from './pages/Contato'
+import Navbar from "./components/Navbar";
 
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional/>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/empresa" element={<Empresa/>}/>
+        <Route path="/contato" element={<Contato/>}/>
+      </Routes>
+    </Router>
   )
+
 }
 
 export default App
